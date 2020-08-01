b62d647e4c61959e69080afddb8913ebbfb6c73b
varzob33 <varzob33@gmail.com>
2020-08-01 15:43:07 -0400

    add Book_a_room.java

--- src/Book_a_Room.java
+++ src/Book_a_Room.java
@@ -2,6 +2,8 @@ public class Book_a_Room {
     public static void main(String[] args) {
         System.out.println("No rooms available ");
 
+        // my code from IntelIJ, hi GitHub
+
     }
 
 }
